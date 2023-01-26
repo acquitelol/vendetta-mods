50826720c31e62a8e693aeb8fedfc2dbf4cdaa2b
Acquite <acquitefx@gmail.com>
2023-01-26 13:41:52 +0000

    push eval plugin

--- src/index.js
+++ src/index.js
@@ -1,12 +1,93 @@
-import { logger } from "@vendetta";
-import Settings from "./Settings";
+import { registerCommand, sendReply } from "@vendetta/commands";
+
+let unregister;
+async function evaluate(src, isAsync = false) {
+    let result, errored;
+    if (isAsync || src.includes("await")) {
+        if (src.includes(";") && (!src.endsWith(";") || src.includes("\n") || (src.split(';').length) > 2)) {
+            src = `(async function() { ${src} })()`;
+        } else {
+            src = `(async function() { return ${src} })()`;
+        }
+    }
+  
+    let start = new Date().getTime();
+    try {
+        result = eval(src);
+        if (result instanceof Promise) {
+            result = await result;
+        }
+    } catch(e) {
+        result = e;
+        errored = true;
+    }
+  
+    let elapsed = new Date().getTime() - start;
+    return [errored, result, elapsed];
+}
 
 export default {
-    onLoad: () => {
-        logger.log("Hello world!");
-    },
-    onUnload: () => {
-        logger.log("Goodbye, world.");
-    },
-    settings: Settings,
-}
+  onLoad: () => {
+    unregister = registerCommand({
+        applicationId: "-1", // Built in category. do not change or it will explod
+
+        name: "eval",
+        displayName: "eval",
+
+        description: "Runs code specified. BE CAREFUL! RUNNING UNVERIFIED CODE HERE CAN LEAD TO YOU BEING TOKEN LOGGED!",
+        displayDescription: "Runs code specified. BE CAREFUL! RUNNING UNVERIFIED CODE HERE CAN LEAD TO YOU BEING TOKEN LOGGED!",
+
+        type: 1, 
+        inputType: 1,
+
+        options: [
+            {
+                name: "type",
+                displayName: "type",
+    
+                description: "The type of the code to run (regular or asynchronous)",
+                displayDescription: "The type of the code to run (regular or asynchronous)",
+    
+                type: 3,
+                required: true,
+
+                choices: [
+                    {
+                        name: "normal",
+                        displayName: "normal",
+                        value: false
+                    },
+                    {
+                        name: "async",
+                        displayName: "async",
+                        value: true
+                    }
+                ]
+            },
+            {
+                name: "source",
+                displayName: "source",
+
+                description: "The source of the code to run",
+                displayDescription: "The source of the code to run",
+
+                type: 3,
+                required: true,
+            },
+        ],
+        execute: async function (args, message) {
+            const type = args.find(o => o.name == "type").value
+            const src = args.find(o => o.name == "source").value
+            const [error, result, elapsed] = await evaluate(src, type);
+            sendReply(message.channel.id, [
+                `${error ? "Failed executing" : "Successfully executed"} in ${elapsed}ms`,
+                `\`\`\`js\n${result}\n\`\`\``
+            ].join('\n'))
+        }
+    });
+  },
+  onUnload: () => {
+    // remove cmd
+    unregister();
+  },
+};
